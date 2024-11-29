e6e204a8cd3b4a687b167b88e07fc030f177ac03
Trey Bastian <2991824+TreyBastian@users.noreply.github.com>
2024-11-29 12:28:53 +0000

    fixed paths and used a link instead of button

--- src/contextMenu.js
+++ src/contextMenu.js
@@ -2,21 +2,23 @@ document.onclick = hideMenu;
 document.oncontextmenu = rightClick;
 
 function hideMenu() {
-    document.getElementById("contextMenu").style.display = "none"
+  document.getElementById("contextMenu").style.display = "none";
 }
 
+/**
+ * @param {MouseEvent} e
+ */
 function rightClick(e) {
-    e.preventDefault();
+  e.preventDefault();
 
-    if (document.getElementById(
-        "contextMenu").style.display == "block")
-        hideMenu();
-    else {
-        let menu = document
-            .getElementById("contextMenu")
+  if (document.getElementById("contextMenu").style.display == "block")
+    hideMenu();
+  else {
+    let menu = document.getElementById("contextMenu");
 
-        menu.style.display = 'block';
-        menu.style.left = e.pageX + "px";
-        menu.style.top = e.pageY + "px";
-    }
+    menu.style.display = "block";
+    menu.style.left = e.pageX + "px";
+    menu.style.top = e.pageY + "px";
+  }
 }
+
